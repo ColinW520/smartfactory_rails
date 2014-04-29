51d615e033e89e64cea0b8d4dd3810bbeee4054d
ColinW520 <colinhagan@gmail.com>
2014-04-28 20:36:01 -0500

    prevent direct editing of dates

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -90,7 +90,7 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:customer_name,
        :customer_email, :description,
-        :price, :paid_for_on, :completed_on, :frame_id)
+        :price, :frame_id)
     end
 
     def set_frames
